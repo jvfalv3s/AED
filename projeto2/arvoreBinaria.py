b47e930d9b1191d100797e8ba91ac83fba342bc3
Joao Alves <jvfalves@gmail.com>
2024-03-23 20:43:49 +0000

    "arvore binaria corrigida"

--- projeto2/arvoreBinaria.py
+++ projeto2/arvoreBinaria.py
@@ -2,7 +2,7 @@
 
 print("Arvore Binaria")
 
-class Node:
+class Node: #classe nó
     def __init__(self, data):
         self.left = None
         self.right = None
@@ -11,17 +11,43 @@ class Node:
     def __str__(self):
         return str(self.data)
         
-class BinaryTree:
+class BinaryTree:  #classe arvore binaria
     def __init__(self, data = None):
         if data:
             node = Node(data)
             self.root = node
+        else:
+            self.root = None
             
-if __name__ == "__main__":
-    tree = BinaryTree(10)
-    tree.root.left = Node(5)
-    tree.root.right = Node(15)
+    def insert(self, value):
+        #inserção de um novo nó na arvore binaria
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(value, self.root)
     
-    print(tree.root)
-    print(tree.root.left)
-    print(tree.root.right)
+    def _insertRecursivamente(self, value, node): #tentativa de inserção recursiva
+        if value < node.data:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert(value, node.left)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert(value, node.right)
+            
+    def inserRandom(self, num): #inserção de valores aleatorios
+        for i in range(num):
+            self.insert(i)
+            
+    def imprimir(self): #impressão da arvore binaria
+        if self.root:
+            self._imprimir(self.root)
+
+    def _imprimirRecursivamente(self, node): #tentativa de impressão recursiva
+        if node is None:
+            self._imprimirRecursivamente(node.left)
+            print(node)
+            self._imprimirRecursivamente(node.right)
